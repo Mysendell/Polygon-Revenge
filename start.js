ac36ec09dae40ff8f4a98a0905ac04d694ae7a12
Kleber <kleber.martinsc@gmail.com>
2023-11-05 17:14:05 -0300

    basic enemy system

--- start.js
+++ start.js
@@ -3,14 +3,17 @@ const titleEl = document.querySelector("#title");
 const placarEl = document.querySelector("#Placar");
 const creditosEl = document.querySelector("#Creditos");
 const battleEl = document.querySelector("battle");
+const healthEl = document.querySelector("health");
+let playerHp = 100;
 
 function startGame() {
-    let playerHp = 100;
     jogarEl.classList.add("invisible");
     titleEl.classList.add("invisible");
     placarEl.classList.add("invisible");
     creditosEl.classList.add("invisible");
     battleEl.classList.remove("invisible");
+    healthEl.innerHTML = `HP: ${playerHp}`;
+    handler();
 }
 
 jogarEl.addEventListener("click", startGame);
