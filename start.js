8d8480de2f5c3a6ce558d2f51dc0b543f69e3ec8
Ingrid Jeanine Goncalves de Oliveira <125323073+ingridJeanine@users.noreply.github.com>
2023-11-20 13:42:08 -0300

    Map selector alpha

--- start.js
+++ start.js
@@ -14,21 +14,36 @@ const speedEl = document.querySelector("#speed");
 const costEl = document.querySelector("#cost");
 const upgDivEl = document.querySelector("#upgrade");
 const upgButtonEl = upgDivEl.querySelector("button");
+const mapsEl = document.querySelectorAll(".map");
+const mapSelectorEl = document.querySelector("#map-select");
 const timer = ms => new Promise(res => setTimeout(res, ms));
 let playerHp = 100;
 let money = 100;
 let turretButtons = [];
+let mapa = 0;
+
+function selectMap(e) {
+    mapa = e.currentTarget.value;
+}
+
+for(let i = 0; i < mapsEl.length; i++) {
+    mapsEl[i].addEventListener("click", (e) => {
+        selectMap(e);
+    })
+}
 
 function startGame() {
     jogarEl.classList.add("invisible");
     titleEl.classList.add("invisible");
     placarEl.classList.add("invisible");
     creditosEl.classList.add("invisible");
-    battleEl.classList.remove("invisible");
-    sideEl.classList.remove("invisible");
+    mapSelectorEl.classList.remove("invisible");
     healthEl.innerHTML = playerHp;
     moneyEl.innerHTML = money;
     handler();
 }
+
+/*battleEl.classList.remove("invisible");
+    sideEl.classList.remove("invisible");*/
 
 jogarEl.addEventListener("click", startGame);
