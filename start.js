7fc7a96b271a46770267c76a756145b1640caca5
Ingrid Jeanine Goncalves de Oliveira <125323073+ingridJeanine@users.noreply.github.com>
2023-11-21 14:23:21 -0300

    Add files via upload

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 const bodyEl = document.querySelector("body");
+const mainEl = document.querySelector("main");
 const jogarEl = document.querySelector("#Jogar");
 const titleEl = document.querySelector("#title");
 const placarEl = document.querySelector("#Placar");
@@ -14,16 +15,35 @@ const speedEl = document.querySelector("#speed");
 const costEl = document.querySelector("#cost");
 const upgDivEl = document.querySelector("#upgrade");
 const upgButtonEl = upgDivEl.querySelector("button");
-const mapsEl = document.querySelectorAll(".map");
+const mapsEl = document.querySelectorAll(".map-button");
 const mapSelectorEl = document.querySelector("#map-select");
+const battlefieldEl = document.querySelector("#map");
 const timer = ms => new Promise(res => setTimeout(res, ms));
 let playerHp = 100;
 let money = 100;
 let turretButtons = [];
-let mapa = 0;
+let mapa;
 
 function selectMap(e) {
     mapa = e.currentTarget.value;
+    mapSelectorEl.classList.add("invisible");
+    switch(+mapa) {
+        case 0:
+            battlefieldEl.src = "imgs/Sketch2.png";
+            break;
+        case 1:
+            battlefieldEl.src = "imgs/Sketch2.png";
+            break;
+        case 2:
+            battlefieldEl.src = "imgs/Sketch2.png";
+            break;
+    }
+    for(let i = 0; i < mapsEl.length; i++) {
+        mapsEl[i].classList.add("invisible");
+    }
+    battleEl.classList.remove("invisible");
+    sideEl.classList.remove("invisible");
+    handler(mapa);
 }
 
 for(let i = 0; i < mapsEl.length; i++) {
@@ -33,17 +53,10 @@ for(let i = 0; i < mapsEl.length; i++) {
 }
 
 function startGame() {
-    jogarEl.classList.add("invisible");
-    titleEl.classList.add("invisible");
-    placarEl.classList.add("invisible");
-    creditosEl.classList.add("invisible");
+    mainEl.classList.add("invisible");
     mapSelectorEl.classList.remove("invisible");
     healthEl.innerHTML = playerHp;
     moneyEl.innerHTML = money;
-    handler();
 }
-
-/*battleEl.classList.remove("invisible");
-    sideEl.classList.remove("invisible");*/
 
 jogarEl.addEventListener("click", startGame);
