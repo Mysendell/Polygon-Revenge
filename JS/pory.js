f91e67fc143b89c2ea070e20e2bebc8407b4f2d7
Leo1551 <141284217+Leo1551@users.noreply.github.com>
2023-11-26 14:21:48 -0300

    Update pory.js

--- JS/pory.js
+++ JS/pory.js
@@ -1,28 +1,43 @@
 let poryEl = document.getElementById('pory');
 let poryZEl = document.querySelector('body');
 let tudoEl = document.querySelectorAll('.tema-claro-escuro');
+let entrePgs = 2;
 
-
-$(poryEl).click(() =>{
-    if (poryZEl.style.backgroundImage == 'url("imgs/474-porygon-z-g.png")'){
+function poryTheme() {
+    if (entrePgs == 1){
         $(poryZEl).css('background-image', 'url(imgs/pory-base.png)');
-        $(tudoEl).css('color', 'rgb(0, 0, 0)');
+        $(poryZEl).css('background-color', 'rgb(255, 255, 255)');
+        entrePgs = 2;
         trocaCor(1);
     }
     else{
         $(poryZEl).css('background-image', 'url(imgs/474-porygon-z-g.png)');
-        $(tudoEl).css('color', 'rgb(255, 255, 255)');
+        $(poryZEl).css('background-color', 'rgb(0, 0, 0)');
+        entrePgs = 1;
         trocaCor(2);
     }
 
-});
+}
+
+
+$(poryEl).click(poryTheme);
 
 function trocaCor(num){
     tudoEl.forEach(letras => {
         if (num == 1)
-            letras.css('color', 'rgb(0, 0, 0)'); 
+            $(letras).css('color', 'rgb(0, 0, 0)'); 
         else
-            letras.css('color', 'rgb(255, 255, 255)');
+            $(letras).css('color', 'rgb(255, 255, 255)');
     });
 }
 
+function ativarPorygon() {
+    if(entrePgs == 1)
+        poryTheme();
+    else
+        poryTheme();
+
+
+}
+
+window.addEventListener('load', ativarPorygon);
