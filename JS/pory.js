7b7113fd5f3ae636aab475800cf5ef659ec6e00e
Leo1551 <141284217+Leo1551@users.noreply.github.com>
2023-11-26 15:21:14 -0300

    Update pory.js

--- JS/pory.js
+++ JS/pory.js
@@ -1,24 +1,22 @@
 let poryEl = document.getElementById('pory');
 let poryZEl = document.querySelector('body');
 let tudoEl = document.querySelectorAll('.tema-claro-escuro');
-let entrePgs = 2;
 
 function poryTheme() {
     if (entrePgs == 1){
-        $(poryZEl).css('background-image', 'url(imgs/pory-base.png)');
-        $(poryZEl).css('background-color', 'rgb(255, 255, 255)');
-        entrePgs = 2;
-        trocaCor(1);
-    }
-    else{
         $(poryZEl).css('background-image', 'url(imgs/474-porygon-z-g.png)');
         $(poryZEl).css('background-color', 'rgb(0, 0, 0)');
-        entrePgs = 1;
+        entrePgs = 2;
         trocaCor(2);
+    }
+    else{
+        $(poryZEl).css('background-image', 'url(imgs/pory-base.png)');
+        $(poryZEl).css('background-color', 'rgb(255, 255, 255)');
+        entrePgs = 1;
+        trocaCor(1);        
     }
 
 }
-
 
 $(poryEl).click(poryTheme);
 
@@ -31,13 +29,3 @@ function trocaCor(num){
     });
 }
 
-function ativarPorygon() {
-    if(entrePgs == 1)
-        poryTheme();
-    else
-        poryTheme();
-
-
-}
-
-window.addEventListener('load', ativarPorygon);
